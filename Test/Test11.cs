1bef67453bcfea09e673d3f1c197dc71b4fae44a
Vandana chouhan <vandanachouhan@arnasoftech.com>
2024-10-25 16:49:55 +0530

    Test11code

---
+++ Test/Test11.cs
@@ -0,0 +1,23 @@
+﻿using JustFoia.Page;
+using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+
+namespace JustFoia.Test
+{
+    public class Test11 : PageTest
+    {
+        private Page11? page11;
+
+        [SetUp]
+        public void setup()
+        {
+            page11 = new Page11(Page);
+        }
+        [Test]
+        public async Task Case11()
+        {
+            await page11!.VerifyTagscanbeDeleted();
+        }
+    }
+
+}
