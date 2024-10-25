46e1cd7a8e81861600ebb241bdc6890e00ce8f88
Vandana chouhan <vandanachouhan@arnasoftech.com>
2024-10-25 15:17:47 +0530

    Test10code

---
+++ Test/Test10.cs
@@ -0,0 +1,22 @@
+﻿using JustFoia.Page;
+using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+
+namespace JustFoia.Test
+{
+    public class Test10 : PageTest
+    {
+        private Page10? _page10;
+        [SetUp]
+        public void Setup()
+        {
+            _page10 = new Page10(Page);
+        }
+        [Test]
+        public async Task Case10()
+        {
+            await _page10!.VerifythattagscanbearchivedAsync();
+        }
+    }
+}
+
